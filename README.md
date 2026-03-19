29fabde4f628dfde40aab63869788b31b66cc9e2
Ryan Wild <ryanwild@users.noreply.github.com>
2026-03-19 15:26:55 +0900

    added merge sort

---
+++ README.md
@@ -0,0 +1,28 @@
+# Learning about Data Structures and Algorithms
+
+A creative space for me to explore DSA in my own time...
+
+## TypeScript
+
+Examples are in the /typescript directory and can be executed as an executable script.
+
+For example:
+
+```bash
+./typescript/merge-sort.ts
+```
+
+Outputs:
+
+```bash
+Before Merge Sort: [
+  6, 6, 5, 7, 6,
+  7, 0, 7, 0, 4
+]
+After: [
+  0, 0, 4, 5, 6,
+  6, 6, 7, 7, 7
+]
+```
+
+* (Note, Node.js and npm are required to run the TypeScript examples)
---
+++ typescript/merge-sort.ts
@@ -0,0 +1,52 @@
+#!/usr/bin/env -S npx tsx
+
+const size = 10
+const testData = Array.from(
+  { length: size },
+  () => (
+    Math.floor(
+      Math.random() * size
+    )
+  ),
+) as number[]
+
+function mergeSort(input: number[]): number[] {
+  // if the input length is 2 we are done
+  // splitting the array
+  if (input.length < 2) {
+    return input
+  }
+  const left = mergeSort(input.slice(0, Math.ceil(input.length / 2)))
+  const right = mergeSort(input.slice(Math.ceil(input.length / 2)))
+  return merge(left, right)
+}
+
+function merge(left: number[], right: number[]): number[] {
+  const result: number[] = []
+  let leftIndex = 0
+  let rightIndex = 0
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] <= right[rightIndex]) {
+      result.push(left[leftIndex])
+      leftIndex++
+    } else {
+      result.push(right[rightIndex])
+      rightIndex++
+    }
+  }
+  // add the left over items from either array
+  while (leftIndex < left.length) {
+    result.push(left[leftIndex])
+    leftIndex++
+  }
+  while (rightIndex < right.length) {
+    result.push(right[rightIndex])
+    rightIndex++
+  }
+  return result
+}
+
+const result = mergeSort(testData)
+console.log("Before Merge Sort:", testData)
+console.log("After:", result)
+
